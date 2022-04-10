1a3344328ff727bb8af94f4dc346513ab736eecc
Bitwise <eniz.h12@gmail.com>
2022-04-10 23:25:39 +0100

    Use global context for driver

--- driver.py
+++ driver.py
@@ -1,6 +1,6 @@
+from Context import ctx
 from Binders.ConfigBinder import bind
 
-from flask import Flask # , session, request, Response
 from flask_cors import CORS
 from flask_session import Session
 
@@ -8,21 +8,20 @@ from json import load
 
 from os import urandom
 
-app = Flask(__name__)
-app.config["DEBUG"] = True
-app.config["SECRET_KEY"] = urandom(64)
-app.config["SESSION_TYPE"] = "filesystem"
-app.config["SESSION_COOKIE_SECURE"] = True
+ctx.app.config["DEBUG"] = True
+ctx.app.config["SECRET_KEY"] = urandom(64)
+ctx.app.config["SESSION_TYPE"] = "filesystem"
+ctx.app.config["SESSION_COOKIE_SECURE"] = True
 
-Session(app)
-CORS(app)
+Session(ctx.app)
+CORS(ctx.app)
 
 config = None
 
 with open("config.json", 'r') as conf_file:
     config = load(conf_file)
 
-bind(app, config)
-print(app.url_map)
+bind(ctx.app, config)
+print(ctx.app.url_map)
 
-app.run()
+ctx.app.run()
